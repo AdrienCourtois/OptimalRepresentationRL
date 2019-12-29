1dfa97fbffbd303cb0df9530b7a1112f6b5879b1
adrien.courtois <adrien.courtois@telecom-paristech.fr>
2019-12-29 17:21:28 +0100

    Adding handling of CUDA for the Policy class

--- models/AVFPolicy.py
+++ models/AVFPolicy.py
@@ -9,6 +9,13 @@ class AVFPolicy(nn.Module):
         self.policy = Variable(torch.rand(MDP.n_states, MDP.n_actions), requires_grad=True)
 
         self.optimizer = torch.optim.Adam([self.policy])
+        self.is_cuda = False
+    
+    def cuda(self):
+        self.is_cuda = True
+        self.policy = self.policy.cuda()
+
+        return self
     
     def get_policy(self): # x \in \mathcal{X}
         return torch.softmax(self.policy, -1)
